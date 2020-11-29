7101bfca0d6ad396e3e9a898ccc279b5b9a5f0fa
Shrey Tiwari <shrey2697@gmail.com>
2020-11-29 10:42:09 +0530

    Feat : (Example Page Development and Designing)

--- static/js/widget.js
+++ static/js/widget.js
@@ -2,15 +2,34 @@
   this.Widget = function() {
     this.html =
       `<div class='container'>` +
-      `<h1>Hello</h1>` +
-      `<button id='submit-button'>Click</button>` +
+      `<div class='discount-text'>Get 10% Cashback on early bookings</div>` +
+      `<div class='logo'><span>b</span><span>l</span><span>a</span><span>h</span>.com</div>` +
+      `<div class="input-row" style="margin-top:5%">` +
+      `    <div class="input-cell">` +
+      `        <label class="input-label" id="input-firstname-label" for="input-firstname">Firstname</label>` +
+      `        <input type="text" id="input-firstname" class="input-input" name="firstname" />` +
+      `    </div>` +
+      `    <div class="input-cell">` +
+      `        <label class="input-label" id="input-lastname-label" for="input-lastname">Lastname</label>` +
+      `        <input type="text" id="input-lastname" class="input-input" name="lastname" />` +
+      `    </div>` +
+      `</div>` +
+      `<div class="input-row">` +
+      `    <div class="input-cell">` +
+      `        <label class="input-label" id="input-email-label" for="input-email">Email ID</label>` +
+      `        <input type="text" id="input-email" class="input-input" name="email" />` +
+      `    </div>` +
+      `    <div class="input-cell">` +
+      `        <label class="input-label" id="input-phone-label" for="input-phone">Phone</label>` +
+      `        <input type="text" id="input-phone" class="input-input" name="phone" />` +
+      `    </div>` +
+      `</div>` +
+      `<button id='submit-button'>BOOK NOW</button>` +
       `</div>`;
     var defaults = {
-      // source: window.location.hostname,
       containerId: "widget",
       formName: "form",
-      // action: "https://signup.blinker.com.au/subscription/create",
-      // apiHost: "https://api.blinker.com.au",
+      // action: "https://signup.input.com.au/subscription/create",
     };
     this.options = defaults;
     initializeWidget(this);
