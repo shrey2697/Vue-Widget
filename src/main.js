b578c4b3b26d7437db3ccbff62a61cee72baf392
Shrey Tiwari <shrey2697@gmail.com>
2020-12-29 22:04:54 +0530

    Feat : (Vue custom element)

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/en";
+import vueCustomElement from "vue-custom-element";
 
+Vue.use(vueCustomElement);
 Vue.config.productionTip = false;
 Vue.use(ElementUI, { locale });
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+// new Vue({
+//   router,
+//   store,
+//   render: (h) => h(App),
+// }).$mount("#app");
+App.store = store;
+App.router = router;
+
+Vue.customElement("widget-loader", App);
